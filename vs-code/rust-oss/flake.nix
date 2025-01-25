8c65493e477c5e85949d1a0b0a0e40b7af25756b
Maximilian Konter <maximilian.konter@protonmail.com>
2025-01-25 20:18:33 +0100

    Add rust-oss (vs-code) template.
    This template provides a VS-code installation with everything you need
    to develop rust.
    Note though, that Rust itself and it's dependencies are not provided and
    must be loaded from a different template.

---
+++ vs-code/rust-oss/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              # Ide
+              (vscode-with-extensions.override {
+                vscode = vscodium;
+                # https://users.rust-lang.org/t/setting-up-rust-with-vs-code/76907
+                vscodeExtensions = with vscode-extensions; [
+                  bbenoist.nix
+                  rust-lang.rust-analyzer
+                  vadimcn.vscode-lldb
+                  catppuccin.catppuccin-vsc
+                  catppuccin.catppuccin-vsc-icons
+                  vscodevim.vim
+                  tamasfe.even-better-toml
+                  jnoortheen.nix-ide
+                  formulahendry.code-runner
+                ];
+              })
+            ];
+          };
+        }
+    );
+}
