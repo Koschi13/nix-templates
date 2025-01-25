04112684bd692daecc0906cd6f493fb8b33c45f5
Maximilian Konter <maximilian.konter@protonmail.com>
2025-01-25 20:27:31 +0100

    Move rust-vs-code into vs-code.
    Remove the Rust stuff and make it a vs-code only template.

--- vs-code/rust/flake.nix
+++ vs-code/rust/flake.nix
@@ -2,40 +2,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
-          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
-            inherit system overlays;
+            inherit system;
             config.allowUnfree = true;
           };
-
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         in
         with pkgs;
         {
           devShells.default = mkShell {
-            nativeBuildInputs = [
-              # Rust
-              rustToolchain
-              pkg-config
-
-              # IDE deps
-              gcc
-            ];
-
             buildInputs = [
               # Ide
               (vscode-with-extensions.override {
@@ -49,23 +29,11 @@
                   vscodevim.vim
                   tamasfe.even-better-toml
                   jnoortheen.nix-ide
-                  ms-vsliveshare.vsliveshare
                   formulahendry.code-runner
+                  ms-vsliveshare.vsliveshare
                 ];
               })
-
-              # Build deps
-              openssl
-              clang
-              lld
-              postgresql
             ];
-
-            LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
-
-            shellHook = ''
-              export PATH="$HOME/.cargo/bin:$PATH"
-            '';
           };
         }
     );
