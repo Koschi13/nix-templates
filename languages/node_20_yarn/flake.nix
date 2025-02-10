1bb047c797d3e17ef668832862913dbffd920808
Maximilian Konter <maximilian.konter@protonmail.com>
2025-02-10 08:25:51 +0100

    Add flakes for development at work.

---
+++ languages/node_20_yarn/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment for JS services";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        # Source definitions
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            yarn
+            nodejs_20
+          ];
+
+          shellHook = ''
+            printf "\nJS environment loaded \n";
+          '';
+        };
+      }
+    );
+}
