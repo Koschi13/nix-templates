fa0927c285633a622ef2028ff152512b3188ebd9
Maximilian Konter <maximilian.konter@protonmail.com>
2025-05-20 06:48:58 +0200

    Split Python311 and PostgreSQL.

---
+++ tools/dev/postgresql_16/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Development environment for PostgreSQL 16";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        # Source definitions
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # Python test deps
+            pkgs.postgresql_16
+            pkgs.postgresql_16.lib
+          ];
+          # Extent the LD_LIBRARY_PATH with the binaries needed by python, so python can find them
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.postgresql_16.lib]}:$LD_LIBRARY_PATH";
+
+          shellHook = ''
+            printf "\nPostgreSQL 16 envrionment \n";
+          '';
+        };
+      }
+    );
+}
